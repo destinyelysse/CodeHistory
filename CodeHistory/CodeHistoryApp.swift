099d15725995d3abfd6ab327ab1cf9b235ff6d96
Destiny Serna <destinyeserna@gmail.com>
2023-03-09 19:38:31 -0800

    Initial Commit

---
+++ CodeHistory/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  CodeHistory
+//
+//  Created by Destiny Serna on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeHistory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeHistory
+//
+//  Created by Destiny Serna on 3/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
